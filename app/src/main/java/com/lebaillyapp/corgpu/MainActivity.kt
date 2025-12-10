99950ef26ace1357084b671743aa0fd4cdc13452
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-10 21:34:17 +0100

    feature(main/): Plug the benchmark screen composition to the activity.

--- app/src/main/java/com/lebaillyapp/corgpu/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/corgpu/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lebaillyapp.corgpu.benchmark.ui.screen.MatrixBenchmarkScreen
+import com.lebaillyapp.corgpu.benchmark.viewmodel.MatrixBenchmarkViewModel
 import com.lebaillyapp.corgpu.ui.theme.CorGPUTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CorGPUTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel: MatrixBenchmarkViewModel = viewModel()
+                    MatrixBenchmarkScreen(viewModel = viewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CorGPUTheme {
-        Greeting("Android")
-    }
-}
