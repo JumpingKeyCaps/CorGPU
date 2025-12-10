c45cd8b17b6e977e722bba7d3fe1dffa1f3fb007
Samuel Lebailly <sam.meusa@gmail.com>
2025-12-10 20:51:49 +0100

    chore(build.gradle/app): Add Vico Charts and lifecycle VM compose dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    // Vico Charts - Compose support (Material 3)
+    implementation("com.patrykandpatrick.vico:compose:2.1.3")
+    implementation("com.patrykandpatrick.vico:compose-m3:2.1.3")
+    implementation("com.patrykandpatrick.vico:core:2.1.3")
+
+
+
 }
